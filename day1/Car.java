d0504c9e84d38e7d45c93e08d96e6e038f3618a2
denraj1505 <114810904+denraj1505@users.noreply.github.com>
2022-10-01 18:09:10 +0530

    Add files via upload
    Day1

---
+++ day1/Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+	byte number=127;
+	short number1=32767;
+	int number2=475136487;
+	long number3=12345678901L;
+	float number4=124357.3F;
+	double number5=1235599799263969.59945599556;
+	char character='a';
+	boolean validation=true;
+	String name="Testleaf";
+	
+
+}
---
+++ day1/Factorial.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int fact=1;  
+		 int test=5;
+		   
+		  for(int i=1;i<=test;i++){    
+			  fact=fact*i;    
+		  }    
+		  System.out.println("Factorial = "+fact);    
+		 }  
+		
+
+	}
+
+
